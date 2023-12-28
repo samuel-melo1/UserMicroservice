2c1aa9e92cb221567b245f3c3ffe3ce80b97af74
samuel-melo1 <samuelprogramacao3@gmail.com>
2023-12-28 00:08:22 -0300

    implemented publish email

--- src/main/java/com/ms/usermicrosrevice/service/UserService.java
+++ src/main/java/com/ms/usermicrosrevice/service/UserService.java
@@ -1,6 +1,7 @@
 package com.ms.usermicrosrevice.service;
 
 import com.ms.usermicrosrevice.models.UserModel;
+import com.ms.usermicrosrevice.producers.UserProducer;
 import com.ms.usermicrosrevice.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,13 +10,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private UserRepository userRepository;
+    private UserProducer userProducer;
 
-    public UserService(UserRepository userRepository){
+    public UserService(UserRepository userRepository, UserProducer userProducer) {
         this.userRepository = userRepository;
+        this.userProducer = userProducer;
     }
 
     @Transactional
-    public UserModel save(UserModel userModel){
-        return userRepository.save(userModel);
+    public UserModel save(UserModel userModel) {
+        userModel = userRepository.save(userModel);
+        userProducer.publishMessageEmail(userModel);
+        return userModel;
     }
 }
