271ab42467afaf264d0185584a778c6f00db9af5
samuel-melo1 <samuelprogramacao3@gmail.com>
2023-12-27 21:23:59 -0300

    initial commit

---
+++ src/test/java/com/ms/usermicrosrevice/UserMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.usermicrosrevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
